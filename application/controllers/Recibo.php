e1a653853efec4e9885cd812e6b42dd34af4fc9a
cfarro09 <fdcarlosd1@gmail.com>
2021-07-24 11:47:50 -0500

    fix

--- application/controllers/Recibo.php
+++ application/controllers/Recibo.php
@@ -42,43 +42,4 @@ class Recibo extends CI_Controller
 		echo json_encode($response);
 
 	}
-
-
-    public function register_boleta(){
-		$idpers = html_purify($this->input->post('idpers'));
-		$fecha = html_purify($this->input->post('fecha'));
-		$apellido_pat = html_purify($this->input->post('apellido_pat'));
-		$apellido_mat = html_purify($this->input->post('apellido_mat'));
-		$nombres = html_purify($this->input->post('nombres'));
-		$dni = html_purify($this->input->post('dni'));
-		$nro_boleta = html_purify($this->input->post('nro_boleta'));
-		$observacion = html_purify($this->input->post('observacion'));
-		$detalle_boleta = html_purify($this->input->post('data_extra'));
-
-		if (($apellido_pat && $apellido_mat && $nombres && $dni && $fecha && $nro_boleta)   || ($apellido_pat == "ANULADO" || $dni == 0)) {
-			$send_boleta = array(
-				'fecha' => $fecha, 
-				'nro_boleta' => (int) $nro_boleta,
-				'nombres' => $nombres,
-				'apellido_mat' => $apellido_mat,
-				'apellido_pat' => $apellido_pat,
-				'dni' => $dni == "0" ? "ANULADO" : $dni,
-				'observacion' => $observacion
-			);
-			$response =  $this->BoletasUnif_model->register_boleta($send_boleta);
-			if ($detalle_boleta) {
-				if (isset($response['success']) && $response['success']) {
-					$detalle_boleta = json_decode($detalle_boleta);
-					$response_1 =  $this->BoletasUnif_model->register_detalle_boleta($detalle_boleta);
-					echo json_encode($response_1);
-				}else{
-					echo json_encode("false");
-				}
-			}else{
-				echo json_encode($response);
-			}
-		}else{
-			echo json_encode("false");
-		}
-	}
 }
